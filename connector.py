65fd375e1f08780fda25a9b73bec22dfc0d321d3
Mustafa Dağkıranlar <dagkiranlar.m@gmail.com>
2022-12-19 10:05:04 +0200

    er diagram updated

---
+++ connector.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+def save_palte(plate,name):
+    connection = mysql.connector.connect(host='localhost',
+                                    database='grad',
+                                    user='root',
+                                    password='Dagkiranlar')
+    cursor = connection.cursor()
+    query = "INSERT INTO detections (text, plate) VALUES (%s,%s)"
+    values = (name,plate,photo)
+    # Execute the query
+    print(cursor.execute(query, values))
+
+    connection.commit()
+
+    connection.close()
+    cursor.close()
+
+def show_data():
+    connection = mysql.connector.connect(host='localhost',
+                                        database='test',
+                                        user='root',
+                                        password='Dagkiranlar')
+    cursor = connection.cursor()
+    query = "select * from table_test"
+    cursor.execute(query)
+    # get all records
+    records = cursor.fetchall()
+    print("Total number of rows in table: ", cursor.rowcount)
+    connection.close()
+    cursor.close()
+    print("\nPrinting each row")
+    for row in records:
+        print("id = ", row[0], )
+        print("text = ", row[1],"\n")
+
+if __name__ == "__main__":
+    show_data()
